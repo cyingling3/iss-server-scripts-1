b44155dc34ba3b6c0a229e69fab06947aee00bd9
cyingling3 <104858984+cyingling3@users.noreply.github.com>
2022-09-27 10:41:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ apt update && apt upgrade -y
 apt install nginx jq curl -y
 # configure nginx
 rm -r /var/www/html/*
-wget -O /var/www/html/index.html https://raw.githubusercontent.com/cyingling3/iss-server-scripts/main/index.html
+wget -O /var/www/html/index.html https://github.com/cyingling3/iss-server-scripts-1/edit/main/install.sh
 # create scripts directory & download scripts
 rm -r /scripts
 mkdir /scripts
-wget -O /scripts/install.sh https://raw.githubusercontent.com/cyingling3/iss-server-scripts/main/install.sh
-wget -O /scripts/update-webpage.sh https://raw.githubusercontent.com/cyingling3/iss-server-scripts/main/update-webpage.sh
+wget -O /scripts/install.sh https://github.com/cyingling3/iss-server-scripts-1/edit/main/install.sh
+wget -O /scripts/update-webpage.sh https://github.com/cyingling3/iss-server-scripts-1/edit/main/install.sh
 # create crontab
 echo "* * * * * sh /scripts/update-webpage.sh" >> /var/tmp/cronconfig
 crontab /var/tmp/cronconfig
